ccb9fa3eeefdfc175cc0a15d84db16f20f3dd3b8
bogdanlotuk <bogdanlotuk@gmail.com>
2022-01-31 15:55:36 +0200

    Solar system

--- 1.Typescript/exercise1.js
+++ 1.Typescript/exercise1.js
@@ -1,18 +1,49 @@
+var GroupOfPlanet;
+(function (GroupOfPlanet) {
+    GroupOfPlanet["FirstGroup"] = "Little planet of the Earth's group";
+    GroupOfPlanet["SecondGroup"] = "One of the giants in the Solar system";
+})(GroupOfPlanet || (GroupOfPlanet = {}));
 var PlanetOfSolarSystem = /** @class */ (function () {
-    function PlanetOfSolarSystem(diameter, life, nameOfPlanet, distance) {
+    function PlanetOfSolarSystem(diameter, life, nameOfPlanet, distance, groupOfPlanet, satellite) {
         this.diameter = diameter;
         this.life = life;
         this.nameOfPlanet = nameOfPlanet;
         this.distance = distance;
+        this.groupOfPlanet = groupOfPlanet;
+        this.satellite = satellite;
     }
     PlanetOfSolarSystem.prototype.showDistance = function () {
-        console.log("This planet ".concat(this.nameOfPlanet, " is ").concat(this.distance, " km from the Sun"));
+        console.log("This planet ".concat(this.nameOfPlanet, " is ").concat(this.distance, " km from the Sun."));
+        if (this.satellite[0]) {
+            console.log("It has ".concat(this.satellite[1], " satellites!"));
+        }
+        else {
+            console.log("It doesn't nave any satellite!");
+        }
     };
+    Object.defineProperty(PlanetOfSolarSystem.prototype, "Info", {
+        get: function () {
+            return this.otherInfo;
+        },
+        set: function (value) {
+            this.otherInfo = value || false;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return PlanetOfSolarSystem;
 }());
-var Jupiter = new PlanetOfSolarSystem(1200, false, 'Jupiter', 3500000);
+var Jupiter = new PlanetOfSolarSystem(1200, false, 'Jupiter', 3500000, GroupOfPlanet.SecondGroup, [true, 20]);
+Jupiter.Info = "It is the biggest planet in our Solar system";
+function weKnowNewPlatet(arg) {
+    console.log("Now we have many info about ".concat(arg, "!"));
+    return "Now we have many info about ".concat(arg, "!");
+}
+weKnowNewPlatet("Jupiter");
 console.log(Jupiter.showDistance());
 console.log(Jupiter.diameter);
-//console.log(Jupiter.distance);
+console.log(Jupiter.distance);
 console.log(Jupiter.life);
-//console.log(Jupiter.nameOfPlanet);
+console.log(Jupiter.nameOfPlanet);
+console.log(Jupiter.Info);
+console.log(Jupiter.groupOfPlanet);
--- 1.Typescript/exercise1.ts
+++ 1.Typescript/exercise1.ts
@@ -1,5 +1,14 @@
+enum GroupOfPlanet {
+    FirstGroup = "Little planet of the Earth's group",
+    SecondGroup = "One of the giants in the Solar system",
+}
+
+type PlanetName = "Mercury" | "Venus" | "Earth" | "Mars" | "Jupiter" | "Saturn" | "Uranus" | "Neptune";
+
+type PlanetSatellite = [boolean, number];
 interface DistanseFromTheSun {
     distance: number;
+    groupOfPlanet: GroupOfPlanet;
     showDistance(): void;
 }
 
@@ -9,23 +18,30 @@ interface Planet extends DistanseFromTheSun {
     nameOfPlanet: string;
 }
 
-type PlanetName = "Mercury" | "Venus" | "Earth" | "Mars" | "Jupiter" | "Saturn" | "Uranus" | "Neptune";
-
 class PlanetOfSolarSystem implements Planet{
     public diameter: number;
     readonly life: boolean;
     nameOfPlanet: PlanetName;
     distance: number;
+    groupOfPlanet: GroupOfPlanet;
+    satellite: PlanetSatellite;
     private otherInfo: any; 
     
-    constructor(diameter: number, life: boolean, nameOfPlanet: PlanetName, distance: number ){
+    constructor(diameter: number, life: boolean, nameOfPlanet: PlanetName, distance: number, groupOfPlanet: GroupOfPlanet, satellite: PlanetSatellite ){
         this.diameter = diameter;
         this.life = life;
         this.nameOfPlanet = nameOfPlanet;
         this.distance = distance;
+        this.groupOfPlanet = groupOfPlanet;
+        this.satellite = satellite;
     }
     showDistance(): void {
-        console.log(`This planet ${this.nameOfPlanet} is ${this.distance} km from the Sun`);
+        console.log(`This planet ${this.nameOfPlanet} is ${this.distance} km from the Sun.`);
+        if(this.satellite[0]){
+            console.log(`It has ${this.satellite[1]} satellites!`)
+        } else {
+            console.log(`It doesn't nave any satellite!`)
+        }
     }
 
     get Info() {
@@ -37,8 +53,15 @@ class PlanetOfSolarSystem implements Planet{
     }
 }
 
-let Jupiter = new PlanetOfSolarSystem(1200, false, 'Jupiter', 3500000);
+let Jupiter = new PlanetOfSolarSystem(1200, false, 'Jupiter', 3500000, GroupOfPlanet.SecondGroup, [true, 20]);
 Jupiter.Info = "It is the biggest planet in our Solar system";
+
+function weKnowNewPlatet<T>(arg: T){
+    console.log(`Now we have many info about ${arg}!`);
+    return `Now we have many info about ${arg}!`;
+}
+
+weKnowNewPlatet("Jupiter");
 
 console.log(Jupiter.showDistance());
 console.log(Jupiter.diameter);
@@ -46,4 +69,8 @@ console.log(Jupiter.distance);
 console.log(Jupiter.life);
 console.log(Jupiter.nameOfPlanet);
 console.log(Jupiter.Info)
+console.log(Jupiter.groupOfPlanet)
+
+
+
 
